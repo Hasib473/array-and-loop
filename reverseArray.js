9eb7486f425461026ef164402aefa9b9dcd9a968
Hasib Al Mamun <hasib.gub221@gmail.com>
2025-07-31 12:23:54 +0600

    array reverse

---
+++ reverseArray.js
@@ -0,0 +1,14 @@
+// use reverse function
+let array=[12,34,56,76];
+array.reverse();
+console.log(array);
+
+
+// use row js code 
+
+let arr=[23,34,56,77,88];
+let reverseArr=[];
+for(let i=arr.length-1; i>=0; i--){
+    reverseArr.push(arr[i]);
+}
+console.log(reverseArr);
